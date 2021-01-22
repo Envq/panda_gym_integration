b4a710cf3a08c8a5dcd56157900397bb9b9a2bbf
TowerUB <enrico.sgarbanti@gmail.com>
2021-01-22 14:17:30 +0100

    update server

--- scripts/server.py
+++ scripts/server.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 import socket, sys
+from math import pi
 from utils.joints_msg import createJointsMsg, createCloseMsg, getJointsMsg
+
+
+def processCode(code):
+    if code == '1':
+        return (0.4, 0.1, 0.4, 1.0, 0, 0, 0)
+    elif code == '2':
+        return (0, -pi/4, 0, -pi/2, 0, pi/3, 0)
+    elif code == '3':
+        return (0.00, -0.25 * pi, 0.00, -0.75 * pi, 0.00, 0.50 * pi, 0.25 * pi)
+    else:
+        return (0, 0, 0, 0, 0, 0, 0)
 
 
 def main():
@@ -19,6 +31,7 @@ def main():
 
     # Create a TCP socket at server side
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # for ignore ack and close socket
         s.bind((HOST, PORT))
         s.listen() # enable to accept connection
         print("TCP server ready")
@@ -26,6 +39,7 @@ def main():
         clientAddr = client[0]
         clientPort = client[1]
         print("TCP server connected to client {}:{}".format(clientAddr, clientPort))
+        print("Press q to close")
 
         # Listen for incoming message from client
         with conn:
@@ -33,21 +47,19 @@ def main():
                 # Get joints message
                 msg = conn.recv(1024)
                 current_joints = getJointsMsg(msg)
+                print("Current Joints: {}".format(current_joints))
+
+                # Process current joints
+                code = input("Get code: ")
 
                 # Check if close
-                close = input("Press q to close: ")
-                if close == 'q':
+                if code == 'q':
                     print("Server closed")
                     conn.send(createCloseMsg())
                     break
 
-                # Read current joints state
-                print("Current Joints: {}".format(current_joints))
-
-                # Process the goal joints and send them
-                goal_joints = list()
-                for i in range(7):
-                    goal_joints.append(input("Joint{}: ".format(i)))
+                # Create the goal joints and send them
+                goal_joints = processCode(code)
                 conn.send(createJointsMsg(goal_joints))
 
 
