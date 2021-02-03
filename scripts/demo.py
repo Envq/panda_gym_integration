5a7f778aed074003ddbeac7edc2d23c1b577ebe8
TowerUB <enrico.sgarbanti@gmail.com>
2021-02-03 18:37:34 +0100

    update demo

--- scripts/demo.py
+++ scripts/demo.py
@@ -7,23 +7,36 @@ import gym, panda_gym
 
 class Actor():
     def __init__(self):
-        self.sign = -1
+        pass
+
+    def _goalAchieved(self, goal, obs, th = 0.01):
+        isAchieved = (obs[0] > (goal[0] - th)) and (obs[0] < (goal[0] + th)) and \
+                     (obs[1] > (goal[1] - th)) and (obs[1] < (goal[1] + th)) and \
+                     (obs[2] > (goal[2] - th)) and (obs[2] < (goal[2] + th))
+        # print_col("GOAL STATE: {}".format(isAchieved), 'FG_RED')
+        if isAchieved:
+            return 1
+        else:
+            return 0
 
     def _getObs(self, current_pose):
         # Get Observation: [p_x, p_y, p_z,  p_g1, p_g2,  v_x, v_y, v_z,  v_g1, v_g2]
-        obs = [current_pose[0], current_pose[1], current_pose[2]]
-        obs = obs + [current_pose[7]/2.0, current_pose[7]/2.0]
-        obs = obs + [0, 0, 0, 0, 0]
+        obs = [ current_pose[0], \
+                current_pose[1], \
+                current_pose[2], \
+                current_pose[7] / 2.0, \
+                current_pose[7] / 2.0 ]
         return obs   
     
     def _getAction(self, obs, time_step):
-        if (time_step % 5) == 0:
-            self.sign = - self.sign
-        delta = self.sign * 0.01
+        if time_step < 5:
+            delta = 0.01
+        else:
+            delta = -0.03
         return [delta, 0, 0, 0]
+    
 
-
-    def getNewPose(self, current_pose, time_step):
+    def getNewPose(self, current_pose, goal, time_step):
         obs = self._getObs(current_pose)
         # print("obs: ", obs)
 
@@ -36,7 +49,7 @@ class Actor():
         y = obs[1] + action[1]
         z = obs[2] + action[2]
         grip = obs[3]*2 + action[3]*2
-        grasp = 0
+        grasp = self._goalAchieved(goal, obs)
         new_pose = [x, y, z, 0, 0, 0, 1, grip, grasp]
         # print("panda new target pose: ", new_pose)
         return new_pose
@@ -67,12 +80,12 @@ def testing(panda):
 
     for i in range(NUM_SCEN):
         # Generate goal
-        g = [start_pose[0] + 0.01 * 4, start_pose[1], start_pose[2]]
-        print_col("Scenario's goal: {}\n".format(g), 'FG_MAGENTA')
+        goal = [start_pose[0] - 0.01 * 4, start_pose[1], start_pose[2]]
+        print_col("Scenario's goal: {}\n".format(goal), 'FG_MAGENTA')
 
         for t in range(LEN_SCEN):
             # Process goal and execute them
-            panda_target = my_actor.getNewPose(current_pose, t)
+            panda_target = my_actor.getNewPose(current_pose, goal, t)
                 
             # Send goal 
             panda.sendGoalState(panda_target)
