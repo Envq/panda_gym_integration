5fb47dc771c568a9dd2cabc3c7f5a3cb8d12d08a
TowerUB <enrico.sgarbanti@gmail.com>
2021-02-04 19:45:24 +0100

    update colors

--- scripts/src/colors.py
+++ scripts/src/colors.py
@@ -1,51 +1,64 @@
+""" This script add color to print"""
+
+Colors = {
+    'FG_DEFAULT': '39',
+    'BG_DEFAULT': '49',
+    'FG_BLACK': '30',
+    'BG_BLACK': '40',
+    'FG_RED': '31',
+    'BG_RED': '41',
+    'FG_GREEN': '32',
+    'BG_GREEN': '42',
+    'FG_YELLOW': '33',
+    'BG_YELLOW': '43',
+    'FG_BLUE': '34',
+    'BG_BLUE': '44',
+    'FG_MAGENTA': '35',
+    'BG_MAGENTA': '45',
+    'FG_CYAN': '36',
+    'BG_CYAN': '46',
+    'FG_WHITE': '37',
+    'BG_WHITE': '47',
+    'FG_BLACK_BRIGHT': '90',
+    'BG_BLACK_BRIGHT': '100',
+    'FG_RED_BRIGHT': '91',
+    'BG_RED_BRIGHT': '101',
+    'FG_GREEN_BRIGHT': '92',
+    'BG_GREEN_BRIGHT': '102',
+    'FG_YELLOW_BRIGHT': '93',
+    'BG_YELLOW_BRIGHT': '103',
+    'FG_BLUE_BRIGHT': '94',
+    'BG_BLUE_BRIGHT': '104',
+    'FG_MAGENTA_BRIGHT': '95',
+    'BG_MAGENTA_BRIGHT': '105',
+    'FG_CYAN_BRIGHT': '96',
+    'BG_CYAN_BRIGHT': '106',
+    'FG_WHITE_BRIGHT': '97',
+    'BG_WHITE_BRIGHT': '107',
+    'BOLD': '1',
+    'FAINT': '2',
+    'UNDERLINE': '4',
+    }
 
 
-def _getColor(colorName):
-    if (colorName == 'FG_DEFAULT'):return '39'
-    elif (colorName == 'BG_DEFAULT'): return '49'
-    elif (colorName == 'FG_BLACK'): return '30'
-    elif (colorName == 'BG_BLACK'): return '40'
-    elif (colorName == 'FG_RED'): return '31'
-    elif (colorName == 'BG_RED'): return '41'
-    elif (colorName == 'FG_GREEN'): return '32'
-    elif (colorName == 'BG_GREEN'): return '42'
-    elif (colorName == 'FG_YELLOW'): return '33'
-    elif (colorName == 'BG_YELLOW'): return '43'
-    elif (colorName == 'FG_BLUE'): return '34'
-    elif (colorName == 'BG_BLUE'): return '44'
-    elif (colorName == 'FG_MAGENTA'): return '35'
-    elif (colorName == 'BG_MAGENTA'): return '45'
-    elif (colorName == 'FG_CYAN'): return '36'
-    elif (colorName == 'BG_CYAN'): return '46'
-    elif (colorName == 'FG_WHITE'): return '37'
-    elif (colorName == 'BG_WHITE'): return '47'
-    elif (colorName == 'FG_BLACK_BRIGHT'): return '90'
-    elif (colorName == 'BG_BLACK_BRIGHT'): return '100'
-    elif (colorName == 'FG_RED_BRIGHT'): return '91'
-    elif (colorName == 'BG_RED_BRIGHT'): return '101'
-    elif (colorName == 'FG_GREEN_BRIGHT'): return '92'
-    elif (colorName == 'BG_GREEN_BRIGHT'): return '102'
-    elif (colorName == 'FG_YELLOW_BRIGHT'): return '93'
-    elif (colorName == 'BG_YELLOW_BRIGHT'): return '103'
-    elif (colorName == 'FG_BLUE_BRIGHT'): return '94'
-    elif (colorName == 'BG_BLUE_BRIGHT'): return '104'
-    elif (colorName == 'FG_MAGENTA_BRIGHT'): return '95'
-    elif (colorName == 'BG_MAGENTA_BRIGHT'): return '105'
-    elif (colorName == 'FG_CYAN_BRIGHT'): return '96'
-    elif (colorName == 'BG_CYAN_BRIGHT'): return '106'
-    elif (colorName == 'FG_WHITE_BRIGHT'): return '97'
-    elif (colorName == 'BG_WHITE_BRIGHT'): return '107'
-    elif (colorName == 'BOLD'): return '1'
-    elif (colorName == 'FAINT'): return '2'
-    elif (colorName == 'UNDERLINE'): return '4'
-    elif (colorName == 'DOUBLE_UNDERLINE'): return '21'
-    elif (colorName == 'BLINK'): return '5'
-    else: return '39'
+def colorize(msg, colorName):
+    s = str()
+    try:
+        s ='\033[1;' + Colors[colorName] + 'm' + msg + '\033[0m'
+    except KeyError:
+        s = '\033[1;' + Colors['FG_DEFAULT'] + 'm' + msg + '\033[0m'
+    return s
 
 
 def print_col(msg, colorName):
-    print('\033[1;' + _getColor(colorName) + 'm' + msg + '\033[0m')
+    print(colorize(msg, colorName))
+
+
+def _testALL():
+    for key, value in Colors.items():
+        print_col("testing...", key)
+
 
 
 if __name__ == "__main__":
-    print_col("prova", 'FG_RED')
+    _testALL()
