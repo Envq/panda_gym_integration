71e297dc5276f9bd87bf85e6dae570b597f6889b
TowerUB <enrico.sgarbanti@gmail.com>
2021-02-08 18:23:30 +0100

    panda_gripper improved

--- scripts/src/panda_gripper.py
+++ scripts/src/panda_gripper.py
@@ -12,7 +12,7 @@ class PandaGripperInterface():
     def __init__(self, startup_homing = False): 
         # Create action clients
         self._client_homing = actionlib.SimpleActionClient('franka_gripper/homing', HomingAction)
-        self._client_move_ = actionlib.SimpleActionClient('franka_gripper/move', MoveAction)
+        self._client_move = actionlib.SimpleActionClient('franka_gripper/move', MoveAction)
         self._client_grasp = actionlib.SimpleActionClient('franka_gripper/grasp', GraspAction)
         self._client_stop = actionlib.SimpleActionClient('franka_gripper/stop', StopAction)
 
@@ -20,23 +20,35 @@ class PandaGripperInterface():
         self._client_homing.wait_for_server()
         self._client_move.wait_for_server()
         self._client_grasp.wait_for_server()
+        self._client_stop.wait_for_server()
 
         # Other
-        self.GRIPPER_OPEN = 0.08  # m
-        self.GRIPPER_CLOSE = 0.00 # m
+        self.MIN_WIDTH = 0.00 # [m] closed
+        self.MAX_WIDTH = 0.08 # [m] opened
+        self.MIN_FORCE = 0.01 # [N]
+        self.MAX_FORCE = 50.0 # [N]
         self._timeout = 10.0
 
         if startup_homing:
             self.homing()
 
-
-    def _safe_width(self, val):
-        if val > self.GRIPPER_OPEN:
-            return self.GRIPPER_OPEN
-        elif val < self.GRIPPER_CLOSE:
-            return self.GRIPPER_CLOSE
+    def _normalize(self, val, min, max):
+        if val < min:
+            return min
+        elif val > max:
+            return max
         else:
             return val 
+
+
+    def safe_force(self, val):
+        print("Force Adjusted!")
+        self._normalize(val, self.MIN_FORCE, self.MAX_FORCE)
+
+
+    def safe_width(self, val):
+        print("Width Adjusted!")
+        self._normalize(val, self.MIN_WIDTH, self.MAX_WIDTH)
 
 
     def setTimeout(self):
@@ -66,7 +78,7 @@ class PandaGripperInterface():
         epsilon_inner = epsilon_inner
         epsilon_outer = epsilon_outer
         speed = speed
-        force = force
+        force = self.safe_force(force)
         # Send goal
         self._client_homing.send_goal_and_wait(goal, rospy.Duration.from_sec(self._timeout))
     
@@ -87,6 +99,12 @@ if __name__ == '__main__':
 
     print("PandaGripperInterface.homing()")
     gripper.homing()
+    rospy.sleep(1)
 
     print("PandaGripperInterface.move()")
-    gripper.move(width=0.07, speed=1.0)
+    gripper.move(width=0.07, speed=0.1)
+    rospy.sleep(1)
+
+    print("PandaGripperInterface.grasp()")
+    gripper.grasp(width=0.07, epsilon_inner=0.02, epsilon_outer=0.02, speed=0.1, force=10)
+    rospy.sleep(1)
